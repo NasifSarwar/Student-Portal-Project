ce22427ce84e7e678e6c2d35eac7df759ce43efb
NasifSarwar <sarwar35-3133@diu.edu.bd>
2023-11-20 16:32:24 +0600

    Footer changes of master.blade.php

--- swe_training_portal-main/resources/views/layouts/master.blade.php
+++ swe_training_portal-main/resources/views/layouts/master.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Training Portal</title>
+	<title>Student Portal</title>
     <link rel="icon" type="image/x-icon" href="{{asset('/images/diu-logo.jpg')}}">
 	<meta name="viewport" content="width=device-width">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,6 +19,8 @@
 	<link rel="stylesheet" type="text/css" href="{{asset('gallery.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('section.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('category.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/HeaderFooter.css')}}">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">  
 	
 	<link href="https://fonts.googleapis.com/css?family=Raleway:400,500,600,700" rel="stylesheet">
 
@@ -59,10 +61,10 @@
 				<div class="container">
 					<div class="top-header-left">
 						<div class="top-header-block">
-							<a href="mailto:info@trainingportal.com" itemprop="email"><i class="fas fa-envelope"></i>info@trainingportal.com</a>
+							<a href="mailto:info@studentportal.com" itemprop="email"><i class="fas fa-envelope"></i>info@studentportal.com</a>
 						</div>
 						<div class="top-header-block">
-							<a href="tel:+9779813639131" itemprop="telephone"><i class="fas fa-phone"></i> +88 01770137655</a>
+							<a href="tel:+9779813639131" itemprop="telephone"><i class="fas fa-phone"></i> +88 01521572275</a>
 						</div>
 					</div>
 					<div class="top-header-right">
@@ -77,8 +79,7 @@
 							</ul>
 						</div>
 						<div class="login-block">
-							<a href="/login">Login /</a>
-							<a href="">Register</a>
+							<a href="/login">Login </a>
 						</div>
 					</div>
 				</div>
@@ -114,155 +115,80 @@
 		</header>
 		<!-- Header Close -->
 		
-		<!-- <div class="banner">
-			<div class="owl-five owl-carousel owl-theme">
-	            <div class="item-video">
-            		<iframe width="100%" height="450" src="https://www.youtube.com/embed/ENVW3uZ3a-4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-            		</iframe>
-	            </div>
-	            <div class="item-video">
-	            	<iframe width="100%" height="450" src="https://www.youtube.com/embed/0bfk90rWV9U" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-	            </div>
-	            <div class="item-video">
-	            	<iframe width="100%" height="450" src="https://www.youtube.com/embed/ktvTqknDobU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-	            </div>
-	            <div class="item-video">
-            		<iframe width="100%" height="450" src="https://www.youtube.com/embed/ENVW3uZ3a-4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-            		</iframe>
-	            </div>
-	            <div class="item-video">
-	            	<iframe width="100%" height="450" src="https://www.youtube.com/embed/0bfk90rWV9U" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-	            </div>
-	            <div class="item-video">
-	            	<iframe width="100%" height="450" src="https://www.youtube.com/embed/ktvTqknDobU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-	            </div>
-          </div>
-		</div> -->
-		<!-- Banner Close -->
+
 		@yield('content')
 		<!-- Latest News CLosed -->
 		<section class="query-section">
 			<div class="container">
-				<p>Any Queries? Ask us a question at<a href="tel:+9779813639131"><i class="fas fa-phone"></i> +8801770137655</a></p>
+				<p>Any Queries? Ask us a question at<a href="tel:+9779813639131"><i class="fas fa-phone"></i> +8801521572275</a></p>
 			</div>
 		</section>
 		<!-- End of Query Section -->
-		<footer class="page-footer" itemprop="footer" itemscope itemtype="http://schema.org/WPFooter">
-			<div class="footer-first-section">
-				<div class="container">
-					<div class="box-wrap" itemprop="about">
-						<header>
-							<h1>about</h1>
-						</header>
-						
 
-						<h4><a href="tel:+9779813639131"><i class="fas fa-phone"></i> +8801770137655</a></h4>
-						<h4><a href="mailto:info@educationpro.com"><i class="fas fa-envelope"></i> info@trainingportal.com</a></h4>
-						<h4><a href=""><i class="fas fa-map-marker-alt"></i>Daffdoil Smart City,Ashulia</a></h4>
-					</div>
+    {{-- The main footer  --}}
+  <footer class="footer">
+  	 <div class="container">
+  	 	<div class="row">
+  	 		<div class="footer-col">
+  	 			<h4>About</h4>
+  	 			<ul>
 
-					<div class="box-wrap">
-						<header>
-							<h1>links</h1>
-						</header>
-						<ul>
-							<li><a href="#">Daffodil International University</a></li>
-							<li><a href="#">Department of Software Engineering,DIU</a></li>
-						
-						</ul>
-					</div>
-
-					<div class="box-wrap">
-						<header>
-							<h1>Special Offer</h1>
-						</header>
+  	 				<li><a href="tel:+9779813639131"><i class="fas fa-phone"></i> +8801521572275</a></li>
+  	 				<li><a href="mailto:info@educationpro.com"><i class="fas fa-envelope"></i> info@studentportal.com</a></li>
+  	 				<li><a href=""><i class="fas fa-map-marker-alt"></i>Daffdoil Smart City,Ashulia</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Links</h4>
+  	 			<ul>
+					<li><a href="#">Daffodil International University</a></li>
+					<li><a href="#">Department of Software Engineering,DIU</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Special Courses</h4>
+  	 			<ul>
+					
+					
+					
+  	 				<li>
 						<div class="recent-course-wrap">
 							<img src="{{asset('images/ui-ux.jpg')}}" alt="Ui/Ux Designing">
 							<a href=""><div class="course-name">
 								<h3>UI/UX Designer courses</h3>
-								<p><span>TK500</span> $400</p>
-							</div></a>
-						</div>
+						</div></a>
+					</li>
+  	 				<li>
 						<div class="recent-course-wrap">
 							<img src="{{asset('images/ui-ux.jpg')}}" alt="Ui/Ux Designing">
 							<a href=""><div class="course-name">
 								<h3>UI/UX Designer courses</h3>
-								<p><span>TK50</span> TK40</p>
-							</div></a>
-						</div>
-					</div>
-
-					<div class="box-wrap">
-						<header>
-							<h1>quick contact</h1>
-						</header>
-						<section class="quick-contact">
-							<input type="email" name="email" placeholder="Your Email*">
-							<textarea placeholder="Type your message*"></textarea>
-							<button>send message</button>
-						</section>
-					</div>
-
-				</div>
-			</div>
-			<!-- End of box-Wrap -->
-			<div class="footer-second-section">
-				<div class="container">
-					<hr class="footer-line">
-					<ul class="social-list">
-						<li><a href=""><i class="fab fa-facebook-square"></i></a></li>
-						<li><a href=""><i class="fab fa-twitter"></i></a></li>
-						<li><a href=""><i class="fab fa-skype"></i></a></li>
-						<li><a href=""><i class="fab fa-youtube"></i></a></li>
-						<li><a href=""><i class="fab fa-instagram"></i></a></li>
-					</ul>
-					<hr class="footer-line">
+						</div></a>
+					</li>
+  	 				
+  	 			</ul>
+  	 		</div>
+  	 		
+			   <div class="footer-col">
+				<h4>Follow Us</h4>
+				<div class="social-links">
+					<a href="#"><i class="fab fa-facebook-f"></i></a>
+					<a href="#"><i class="fab fa-twitter"></i></a>
+					<a href="#"><i class="fab fa-instagram"></i></a>
+					<a href="#"><i class="fab fa-linkedin-in"></i></a>
 				</div>
 			</div>
 			<div class="footer-last-section">
 				<div class="container">
-					<p>Copyright 2023 &copy; SWE Department <span> | </span> Website developed by <a href="https://labtheme.com">Daffodil International University</a></p>
+					<p>----------------------------------------->Copyright 2023 &copy; SWE Department <span> | </span> Website developed by <a href="https://labtheme.com">Daffodil International University</a><-------------------------------------------------</p>
 				</div>
 			</div>
-		</footer>
+  	 	</div>
+  	 </div>
+  </footer>
+    {{--End of the main footer  --}}
 
-		<!-- <nav id="menu">
-			<ul>
-				<li><a href="#">HOME</a></li>
-				<li>
-					<span>COURSES</span>
-					<ul>
-						<li><a href="#/courses/child">Child</a></li>
-						<li><a href="#/courses/child">Child</a></li>
-						<li>
-							<span>Child</span>
-							<ul>
-								<li><a href="#/courses/child/grandChild">Grand Child</a></li>
-								<li><a href="#/courses/child/grandChild">Grand Child</a></li>
-							</ul>
-						</li>
-					</ul>
-				</li>
-				<li>
-					<a href="#">gallery</a>
-					<ul>
-						<li><a href="#">Child</a></li>
-						<li><a href="#">Child</a></li>
-						<li><a href="#">Child</a></li>
-					</ul>
-				</li>
-				<li>
-					<a href="#">news</a>
-					<ul>
-						<li><a href="#">Child</a></li>
-						<li><a href="#">Child</a></li>
-						<li><a href="#">Child</a></li>
-					</ul>
-				</li>
-				<li><a href="#">about</a></li>
-				<li><a href="#">contact</a></li>
-			</ul>
-		</nav> -->
+
 
 	</div>
 	<script type="text/javascript" src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
